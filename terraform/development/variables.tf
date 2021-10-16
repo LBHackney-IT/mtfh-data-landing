e7a05464b0242470acb49432b52617c6abd31c82
Mehdi Kimakhe <mehdi.kimakhe@amido.com>
2021-10-16 16:02:11 +0100

    Added SNS subscription and SQS queues to handle person domain events.

--- terraform/development/variables.tf
+++ terraform/development/variables.tf
@@ -1,11 +1,9 @@
 variable "environment_name" {
-    type = string
+    type    = string
     default = "development"
 }
 
 variable "project_name" {
-    type = string
-
-    ### TODO - Update with the require project name. E.g. "Housing-Development"
-    default = "YOUR PROJECT NAME HERE" 
+    type    = string
+    default = "Housing-Development" 
 }
